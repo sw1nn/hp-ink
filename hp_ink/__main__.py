1be99fbf101a72b7f5d0084e70425b682bb989d2
Neale Swinnerton <neale@sw1nn.com>
2024-06-03 08:48:43 +0100

    Add zeroconf discovery and color output

--- hp_ink/__main__.py
+++ hp_ink/__main__.py
@@ -3,6 +3,10 @@ from xml.etree import ElementTree
 
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from zeroconf import ServiceBrowser, ServiceListener, Zeroconf, IPVersion
+import socket
+import time
+from termcolor import colored,cprint
 
 ns = {'ccdyn': 'http://www.hp.com/schemas/imaging/con/ledm/consumableconfigdyn/2007/11/19',
       'dd': 'http://www.hp.com/schemas/imaging/con/dictionaries/1.0/',
@@ -19,23 +23,43 @@ def print_printer_info(hostname):
     serial_number = root.find('.//prdcfgdyn:ProductInformation/dd:SerialNumber',ns)
 
     header_line = f"{make_and_model.text} ({serial_number.text})"
-    print(f'\033[4m{header_line}\033[0m')
-    
+    cprint(header_line, attrs=['bold','underline'])
+
 
 def print_consumable_info(hostname):
     response = requests.get(f"https://{hostname}/DevMgmt/ConsumableConfigDyn.xml", verify=False)
     response.raw.decode_content = True
 
     root = ElementTree.fromstring(response.content)
-    
+
     for n in root.findall(".//ccdyn:ConsumableInfo",ns):
         code = n.find ('dd:ConsumableLabelCode',ns)
         level = n.find('dd:ConsumablePercentageLevelRemaining', ns)
         descriptor = n.find('dd:ConsumableKeyingDescriptor',ns)
         if not (code is None or level is None):
-            print(f"{code.text:<4} {level.text:>2}% {descriptor.text}")
+            status_line = f"{code.text:<4} {level.text:>2}% {descriptor.text}".replace("CMY",f"{colored('C','cyan')}{colored('M','magenta')}{colored('Y', 'yellow')}").replace("K", colored('K','black'))
+            print(status_line)
+
+
+class PrinterListener(ServiceListener):
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        pass
+
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        pass
+
+    def add_service(self, zc: Zeroconf, type: str, name: str) -> None:
+        info = zc.get_service_info(type, name)
+        address=socket.inet_ntoa(info.addresses[0])
+        print_printer_info(address)
+        print_consumable_info(address)
 
 
 if __name__ == "__main__":
-    print_printer_info("hp3b6bda")
-    print_consumable_info("hp3b6bda")
+    zeroconf = Zeroconf()
+    listener = PrinterListener()
+    browser = ServiceBrowser(zeroconf, "_ipp._tcp.local.", listener)
+    try:
+        time.sleep(5)
+    finally:
+        zeroconf.close()
